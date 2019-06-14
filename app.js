c5ea8a7424c7d1980a6534d0a778060689436ba2
J-Kupfer <julian94kupfer@gmail.com>
2019-06-14 14:40:06 +0200

    enable rendering ejs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+path = require('path');
+//Enable css folder to use
+app.use(express.static(__dirname + './views/css'));
+//View enginge to render .ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 //Connect to MongoDB
 mongoose.connect('mongodb+srv://admin:adminPassword@cluster0-mf5vb.mongodb.net/test?retryWrites=true&w=majority',
                 { useNewUrlParser: true },
@@ -15,6 +21,11 @@ app.get('/Home', (req, res) =>{
     res.send('We are on Home');
 });
 
+app.get('/index', function(req, res) {
+    res.render('index');
+  });
+  
 
-//Server hört auf Port 3000
+
+//Server hört auf Port 3000  //localhost:3000
 app.listen(3000);

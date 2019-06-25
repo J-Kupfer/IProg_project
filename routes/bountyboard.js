995f4673ccb13b9ef6aff0ec6aeb62d77a0bd545
xXphilipp420Xx <48790552+xXphilipp420Xx@users.noreply.github.com>
2019-06-25 10:22:01 +0200

    Updatet Get methods leaderboard/bountyboard

--- routes/bountyboard.js
+++ routes/bountyboard.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('./../config/auth');
+const Post = require('../models/Posts');
+const mongoose = require('mongoose');
 
-router.get('/', ensureAuthenticated, (req,res) => res.render('BountyBoard', {
-    user: req.user.name
-}));
+router.get('/', ensureAuthenticated, 
+        function(req, res, next) {Post.find().then(function(doc) {res.render('BountyBoard', {items: doc});});}
+);
+
+//erhalten der Daten von MongoDB und freigeben
+//router.get('/', (req,res) => res.render('BountyBoard', {user: req.user.name}),
+//);
+
+//(req,res) => res.render('BountyBoard', {user: req.user.name}),
 
 module.exports = router;
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated} = require('./../config/auth');
+const User = require('../models/Users');
 
-router.get('/', ensureAuthenticated, (req,res) => res.render('leaderboard', {
+/*router.get('/', ensureAuthenticated, (req,res) => res.render('leaderboard', {
     user: req.user.name
-}));
+}));*/
+
+router.get('/', ensureAuthenticated, 
+        function(req, res, next) {User.find().then(function(doc) {res.render('leaderboard', {items: doc});});}
+);
 
 module.exports = router;
